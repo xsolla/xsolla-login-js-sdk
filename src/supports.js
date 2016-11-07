379b2799c4b047e2a23e93ef7966aee5ee004a89
Arseny Korotaev <a.korotaev@xsolla.com>
2016-11-07 12:11:07 +0500

    Add IE11 support

---
+++ src/supports.js
@@ -0,0 +1,9 @@
+/**
+ * Created by a.korotaev on 07.11.16.
+ */
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position) {
+        position = position || 0;
+        return this.indexOf(searchString, position) === position;
+    };
+}
